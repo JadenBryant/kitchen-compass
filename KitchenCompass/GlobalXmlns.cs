baf5d4fbe7da0c96492231aafade803a9a343c11
Jaden Bryant <jadenbryantx@gmail.com>
2025-09-26 12:16:12 -0700

    Created .NET MAUI project

---
+++ KitchenCompass/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "KitchenCompass")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "KitchenCompass.Pages")]
