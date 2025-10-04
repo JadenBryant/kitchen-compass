52b2aefad3206c69f7eff13b85558efea745f2fb
Jaden Bryant <jadenbryantx@gmail.com>
2025-10-03 20:19:16 -0700

    Project initialization files

---
+++ kitchen-compass-sln/CustomerBackend/Platforms/Android/PlatformClass1.cs
@@ -0,0 +1,5 @@
+﻿namespace CustomerBackend;
+
+// All the code in this file is only included on Android.
+public class PlatformClass1 {
+}
---
+++ kitchen-compass-sln/CustomerBackend/Platforms/Windows/PlatformClass1.cs
@@ -0,0 +1,5 @@
+﻿namespace CustomerBackend;
+
+// All the code in this file is only included on Windows.
+public class PlatformClass1 {
+}
