4de8b7077283f4967afd40e5324a1111726333ec
Douglas Bono <douglas.bono@wsu.edu>
2025-10-03 19:17:54 -0700

    code for menu

--- kitchen-compass-sln/UnitTests/UnitTest1.cs
+++ kitchen-compass-sln/UnitTests/UnitTest1.cs
@@ -1,15 +1,27 @@
-﻿namespace UnitTests;
+﻿using CustomerBackend;
+using CustomerBackend.Menu;
+
+namespace UnitTests;
 
 public class Tests
 {
     [SetUp]
     public void Setup()
     {
+        
     }
 
     [Test]
-    public void Test1()
+    public void TestMenu()
     {
-        Assert.Pass();
+        string menuType = "menutest";
+        string menuId = "1";
+
+        MenuItem testItem = new MenuItem();
+        
+        Menu testMenu = new Menu(menuType, menuId);
+        testMenu.AddItem(testItem);
+
+        Assert.That(testMenu.Items.Count() == 1);
     }
 }
