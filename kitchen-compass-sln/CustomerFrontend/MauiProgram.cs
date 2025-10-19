d4026ab780695db7406f16a63ef77b9b70edff57
Jaden Bryant <jadenbryantx@gmail.com>
2025-10-18 18:51:03 -0700

    Reset MauiProgram.cs to before first Firebase integration attempt.

--- kitchen-compass-sln/CustomerFrontend/MauiProgram.cs
+++ kitchen-compass-sln/CustomerFrontend/MauiProgram.cs
@@ -1,31 +1,16 @@
-﻿using Microsoft.Maui;
+﻿namespace CustomerFrontend;
+
 using Microsoft.Maui.Hosting;
-using Microsoft.Maui.LifecycleEvents;
-using Plugin.Firebase.Auth;        // for CrossFirebaseAuth.Current
-using CustomerFrontend;
-// no Plugin.Firebase.Core needed if you use native init
 
-public static class MauiProgram
-{
-    public static MauiApp CreateMauiApp()
-    {
-        var builder = MauiApp.CreateBuilder()
-            .UseMauiApp<App>();
-
-        builder.ConfigureLifecycleEvents(events =>
-        {
-#if IOS
-            events.AddiOS(iOS => iOS.WillFinishLaunching((_, __) =>
-            {
-                // Initialize Firebase using the native iOS binding
-                global::Firebase.Core.App.Configure();   // reads GoogleService-Info.plist
-                return false;
-            }));
-#endif
-        });
-
-        // DI for auth
-        builder.Services.AddSingleton(_ => CrossFirebaseAuth.Current);
+public static class MauiProgram {
+    public static MauiApp CreateMauiApp() {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts => {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
 
         return builder.Build();
     }
