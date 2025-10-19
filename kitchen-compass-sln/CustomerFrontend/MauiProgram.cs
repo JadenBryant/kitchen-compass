cbfde7ae9d1bad7ad8430b5ac3db9322401ef9c5
Jaden Bryant <jadenbryantx@gmail.com>
2025-10-18 19:59:55 -0700

    Initial setup of Firebase Auth.

--- kitchen-compass-sln/CustomerFrontend/MauiProgram.cs
+++ kitchen-compass-sln/CustomerFrontend/MauiProgram.cs
@@ -1,6 +1,18 @@
 ﻿namespace CustomerFrontend;
 
 using Microsoft.Maui.Hosting;
+using Microsoft.Maui.LifecycleEvents;
+
+#if ANDROID || IOS
+using Plugin.Firebase.Auth;
+#endif
+
+
+#if IOS
+using Plugin.Firebase.Core.Platforms.iOS;
+#elif ANDROID
+using Plugin.Firebase.Core.Platforms.Android;
+#endif
 
 public static class MauiProgram {
     public static MauiApp CreateMauiApp() {
@@ -12,6 +24,29 @@ public static class MauiProgram {
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
             });
 
+#if ANDROID || IOS
+        builder.RegisterFirebaseServices();
+#endif
+
         return builder.Build();
     }
+    
+#if ANDROID || IOS
+    private static MauiAppBuilder RegisterFirebaseServices(this MauiAppBuilder builder) {
+        builder.ConfigureLifecycleEvents(events => {
+#if IOS
+            events.AddiOS(iOS => iOS.WillFinishLaunching((_, __) => {
+                CrossFirebase.Initialize();
+                return false;
+            }));
+#elif ANDROID
+            events.AddAndroid(android => android.OnCreate((activity, _) =>
+                CrossFirebase.Initialize(activity)));
+#endif
+        });
+
+        builder.Services.AddSingleton(_ => CrossFirebaseAuth.Current);
+        return builder;
+    }
+#endif
 }
