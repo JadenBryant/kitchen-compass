1f50fa6ff023ff533a55d1b72e87ffb5eb74ab08
Douglas Bono <douglas.bono@wsu.edu>
2025-10-23 22:14:59 -0700

    Implemented methods to edit any menu item's field, add an item to the menu, remove a menu item, copy a menu, and create a new menu.

--- kitchen-compass-sln/CustomerBackend/Menu/Menu.cs
+++ kitchen-compass-sln/CustomerBackend/Menu/Menu.cs
@@ -14,15 +14,14 @@ namespace CustomerBackend
 
         public string MenuType { get; set; }
         public string Id { get; set; }
-        public Menu(string menutype, string id)
+        public Menu(string menuType, string id)
         {
-            MenuType = menutype;
+            MenuType = menuType;
             Id = id;
         }
-
-        public void AddItem(MenuItem newitem)
+        private void AddItem(MenuItem newItem)
         {
-            Items.Add(newitem);
+            Items.Add(newItem);
         }
 
         public void DisplayMenu()
@@ -38,6 +37,69 @@ namespace CustomerBackend
             {
                 Console.WriteLine("no menu items");
             }
+        }
+
+        public Menu CreateMenu(string menuType, string id)
+        {
+            return new Menu(menuType, id);
+        }
+
+        public void EditMenuItemPrice(Guid id, float newPrice)
+        {
+            MenuItem itemToEdit = Items.First(item => item.Id == id);
+            itemToEdit.Price = newPrice;
+        }
+
+        public void EditMenuItemName(Guid id, string newName)
+        {
+            MenuItem itemToEdit = Items.First(item => item.Id == id);
+            itemToEdit.Name = newName;
+        }
+
+        public void EditMenuItemDescription(Guid id, string newDescription)
+        {
+            MenuItem itemToEdit = Items.First(item => item.Id == id);
+            itemToEdit.Description = newDescription;
+        }
+
+        public void EditMenuItemIngredients(Guid id, string newIngredients)
+        {
+            MenuItem itemToEdit = Items.First(item => item.Id == id);
+            itemToEdit.Ingredients = newIngredients;
+        }
+
+        public void EditMenuItemCalorieCount(Guid id, int newCalorieCount)
+        {
+            MenuItem itemToEdit = Items.First(item => item.Id == id);
+            itemToEdit.CalorieCount = newCalorieCount;
+        }
+
+        public void AddItemToMenu(MenuItem newItem, Menu menu)
+        {
+            foreach (MenuItem existingitem in menu.Items)
+            {
+                if (newItem.Id == existingitem.Id)
+                {
+                    break;
+                }
+                menu.AddItem(newItem);
+            }
+        }
+
+        public void RemoveMenuItem(Guid id)
+        {
+            MenuItem itemToRemove = Items.First(item => item.Id == id);
+            Items.Remove(itemToRemove);
+        }
+
+        public Menu CopyMenu(Menu oldMenu, Menu newMenu)
+        {
+            if (newMenu == null)
+            {
+                newMenu = oldMenu;
+            }
+
+            return newMenu;
         }
     }
 }
