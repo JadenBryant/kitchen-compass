9c8580dfd86643845385add2bace5d5519a5ae0d
Douglas Bono <douglas.bono@wsu.edu>
2025-10-23 22:42:16 -0700

    Created test cases to show that the methods work.

--- kitchen-compass-sln/UnitTests/UnitTest1.cs
+++ kitchen-compass-sln/UnitTests/UnitTest1.cs
@@ -2,25 +2,111 @@
 
 namespace UnitTests;
 
-public class Tests
+public class MenuTests
 {
+    string itemName;
+    float itemPrice;
+    string menuType;
+    string menuId;
+    MenuItem testItem;
+    Menu? testMenu;
+
     [SetUp]
     public void Setup()
     {
-        
+        itemName = "item1";
+        itemPrice = 2;
+        menuType = "menutest";
+        menuId = "1";
+
+        testItem = new MenuItem(itemName, itemPrice);
+        testMenu = new Menu(menuType, menuId);
     }
 
     [Test]
-    public void TestMenu()
+    public void AddItemToMenuTest()
+    {
+        MenuItem testItem = new MenuItem("TestItem", 0);
+        
+        Menu testMenu = new Menu(menuType, menuId);
+        testMenu.AddItemToMenu(testItem, testMenu);
+
+        Assert.That(testMenu.Items.Count() == 1);
+    }
+
+    [Test]
+    public void CreateMenuTest()
     {
         string menuType = "menutest";
         string menuId = "1";
 
-        MenuItem testItem = new MenuItem("TestItem", 0);
-        
-        Menu testMenu = new Menu(menuType, menuId);
-        testMenu.AddItem(testItem);
+        Menu testNewMenu = new Menu(menuType, menuId);
 
-        Assert.That(testMenu.Items.Count() == 1);
+        Assert.That(testNewMenu, Is.Not.Null);
+    }
+
+    [Test]
+    public void EditMenuItemPriceTest()
+    {
+        testMenu.AddItemToMenu(testItem, testMenu);
+        testMenu.EditMenuItemPrice(testItem.Id, 3);
+
+        Assert.That(testMenu.Items[0].Price, Is.EqualTo(3));
+    }
+
+    [Test]
+    public void EditMenuItemNameTest()
+    {
+        testMenu.AddItemToMenu(testItem, testMenu);
+        testMenu.EditMenuItemName(testItem.Id, "item2");
+
+        Assert.That(testMenu.Items[0].Name, Is.EqualTo("item2"));
+    }
+
+    [Test]
+    public void EditMenuItemDescriptionTest()
+    {
+        testMenu.AddItemToMenu(testItem, testMenu);
+        testMenu.EditMenuItemDescription(testItem.Id, "this is item 2");
+
+        Assert.That(testMenu.Items[0].Description, Is.EqualTo("this is item 2"));
+    }
+
+    [Test]
+    public void EditMenuItemIngredientsTest()
+    {
+        testMenu.AddItemToMenu(testItem, testMenu);
+        testMenu.EditMenuItemIngredients(testItem.Id, "cheese");
+
+        Assert.That(testMenu.Items[0].Ingredients, Is.EqualTo("cheese"));
+    }
+
+    [Test]
+    public void EditMenuItemCalorieCountTest()
+    {
+        testMenu.AddItemToMenu(testItem, testMenu);
+        testMenu.EditMenuItemCalorieCount(testItem.Id, 30);
+
+        Assert.That(testMenu.Items[0].CalorieCount, Is.EqualTo(30));
+    }
+
+    [Test]
+    public void RemoveMenuItemTest()
+    {
+        testMenu.AddItemToMenu(testItem, testMenu);
+        testMenu.RemoveMenuItem(testItem.Id);
+
+        Assert.That(testMenu.Items, Is.Empty);
+    }
+
+    [Test]
+    public void CopyMenuTest()
+    {
+        Menu NewMenu = new Menu("menu2", "id2");
+        testMenu.AddItemToMenu(testItem, testMenu);
+
+        NewMenu.CopyMenu(testMenu, NewMenu);
+
+        Assert.That(NewMenu, Is.EqualTo(testMenu));
     }
 }
