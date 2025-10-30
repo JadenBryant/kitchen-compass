6fbface02fd5ac4fb266aa1d52bf407dab6ecbdc
Ryaan101 <ryeryedavis02@gmail.com>
2025-10-30 13:52:20 -0700

    Completed Shopping Cart backend integration. Created cart service, interface, controller routes (add/remove/get/clear), wired DI in Program.cs, and verified functionality in Swagger & Docker.

---
+++ kitchen-compass-sln/CustomerBackend/Services/IShoppingCartService.cs
@@ -0,0 +1,16 @@
+﻿using CustomerBackend.Domain.Cart;
+using CustomerBackend.Domain.Menu;
+using System.Collections.Generic;
+
+namespace CustomerBackend.Services
+{
+    public interface IShoppingCartService
+    {
+        void Add(IMenuItem item, int quantity);
+        void Remove(IMenuItem item, int quantity);
+        void Clear();
+        float Subtotal();
+        List<CartLine> Items();
+    }
+
+}
