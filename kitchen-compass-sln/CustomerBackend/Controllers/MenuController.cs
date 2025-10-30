66ee82b08f5212fc3056b5c3910111bbe97aac1f
Jaden Bryant <jadenbryantx@gmail.com>
2025-10-29 17:40:54 -0700

    Add MenuController to handle menu retrieval

---
+++ kitchen-compass-sln/CustomerBackend/Controllers/MenuController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CustomerBackend.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class MenuController : ControllerBase {
+    [HttpGet]
+    public IActionResult GetMenu() {
+        var items = new[] {
+            new { Id = 1, Name = "Latte", Price = 4.50 },
+            new { Id = 2, Name = "Croissant", Price = 3.25 }
+        };
+
+        return Ok(items);
+    }
+}
